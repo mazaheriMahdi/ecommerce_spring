b44cba584c95e9d86416466f0c159d1608446538
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-05-24 23:44:14 +0330

    finish review end point

--- src/main/java/com/ui/ac/shop/ir/shop/controller/ReviewController.java
+++ src/main/java/com/ui/ac/shop/ir/shop/controller/ReviewController.java
@@ -3,16 +3,13 @@ package com.ui.ac.shop.ir.shop.controller;
 import com.ui.ac.shop.ir.shop.Service.ProductService;
 import com.ui.ac.shop.ir.shop.Service.ReviewService;
 import com.ui.ac.shop.ir.shop.Service.UserService;
-import com.ui.ac.shop.ir.shop.model.ResponseModels.MessageResponseModel;
 import com.ui.ac.shop.ir.shop.model.Review;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/product")
@@ -20,19 +17,27 @@ public class ReviewController {
     ReviewService reviewService;
     UserService userService;
     ProductService productService;
+
     @Autowired
     public ReviewController(ReviewService reviewService, UserService userService, ProductService productService) {
         this.reviewService = reviewService;
         this.userService = userService;
         this.productService = productService;
     }
+
     @GetMapping(value = "/{productId}/review")
-    public ResponseEntity<MessageResponseModel> addReview(
-            @RequestParam(name = "content") String review ,
+    public ResponseEntity<List<Review>> addReview(
+            @RequestParam(name = "content", defaultValue = "") String review,
             @PathVariable Long productId) {
         UserCheckController.CHECK_USER_LOGIN();
-        reviewService.addReview(new Review(review , productService.getProductById(productId) , userService.getCurrentUser()));
-        return new  ResponseEntity<MessageResponseModel>(new MessageResponseModel("Review added"),HttpStatus.OK);
+        List<Review> reviews;
+        if (review.equals("")) {
+            reviews = reviewService.getReviewByProductId(productId);
+        } else {
+            Review temp = new Review(review, productService.getProductById(productId), userService.getCurrentUser());
+            reviewService.addReview(temp);
+            reviews = List.of(temp);
+        }
+        return new ResponseEntity<>(reviews, HttpStatus.OK);
     }
-
 }
