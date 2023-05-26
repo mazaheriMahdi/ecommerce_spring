a9eae55053bf425ac76a4735833869b7a7344af9
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-05-26 05:55:14 +0330

    add CustomerController

---
+++ src/main/java/com/ui/ac/shop/ir/shop/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package com.ui.ac.shop.ir.shop.controller;
+
+
+import com.ui.ac.shop.ir.shop.Service.CustomerService;
+import com.ui.ac.shop.ir.shop.model.Customer;
+import com.ui.ac.shop.ir.shop.model.ResponseModels.GetProfileResponseModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/profile")
+public class CustomerController {
+    CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<GetProfileResponseModel> getProfile(@RequestAttribute("customer") Customer customer){
+        GetProfileResponseModel profileResponseModel = new GetProfileResponseModel(
+                customer.getUser().getName(),
+                customer.getUser().getEmail(),
+                customer.getCredit(),
+                customer.getAvatarUrl()
+        );
+
+        return new ResponseEntity<>(profileResponseModel , HttpStatus.OK);
+    }
+
+
+}
